c043478c40f54647e4dbf1b88284921b76cb8060
Nizam <niju@example.com>
2026-07-11 15:01:51 +0530

    atlas implementation

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 function connection(){
-    const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/website";
+    const mongoURI = process.env.MONGO_URI;
     mongoose.connect(mongoURI)
 .then(()=>{
     console.log("Database Connected");
